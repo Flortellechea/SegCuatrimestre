04b3e32d7ec45343694dabb30c8362ac221a60e7
Flortellechea <55556325+joseyler@users.noreply.github.com>
2024-10-13 18:32:57 -0300

    correcciones entregable

--- entregable/registroAutomotor.ts
+++ entregable/registroAutomotor.ts
@@ -7,30 +7,55 @@ export class RegistroAutomotor {
       this.vehiculos = [];
     }
 
-   getVehiculos(): Vehiculo[] { return this.vehiculos; }
-
-   agregarVehiculo(vehiculo: Vehiculo): void {
-        if (!this.vehiculos.some(v => v.getPatente() === vehiculo.getPatente())) {
-            this.vehiculos.push(vehiculo);
-        } else {
-            console.log(`El vehiculo ${vehiculo.getPatente()} ya se encuentra en el registro`);
-        }
-    }
-
-   modificarVehiculo(vehiculo: Vehiculo): void {
-        const indiceArreglo = this.vehiculos.findIndex(v => v.getPatente() === vehiculo.getPatente());
-     if (indiceArreglo >= 0) {
-            this.vehiculos[indiceArreglo] = vehiculo;
-   } else {
-            console.log(`El vehiculo ${vehiculo.getPatente()} no se encuentra en el registro`);
+    getVehiculos(): Vehiculo[] {
+      return this.vehiculos;
    }
-}
-   eliminarVehiculo(vehiculo: Vehiculo): void {
-        const indiceArreglo = this.vehiculos.findIndex(v => v.getPatente() === vehiculo.getPatente());
-        if (indiceArreglo >= 0) {
-            this.vehiculos.splice(indiceArreglo, 1);
-        } else {
-            console.log(`El vehiculo ${vehiculo.getPatente()} no se encuentra en el registro`);
-        }
-    }
+
+   agregarVehiculo(vehiculo: Vehiculo):void {
+      let indiceArreglo = -1;
+      for (let index = 0; index < this.vehiculos.length; index++) {
+         const existente = this.vehiculos[index];
+         if (existente.getPatente() == vehiculo.getPatente()) {
+            indiceArreglo = index;
+         }         
+      }
+      //no puedo agregar un vehiculo que ya existe
+      if (indiceArreglo == -1) {
+         this.vehiculos.push(vehiculo);
+      } else {
+         console.log(`El vehiculo ${vehiculo.getPatente()} ya se encuentra en el registro`);
+      }
+   }
+
+   modificarVehiculo(vehiculo: Vehiculo):void {
+      let indiceArreglo = -1;
+      for (let index = 0; index < this.vehiculos.length; index++) {
+         const existente = this.vehiculos[index];
+         if (existente.getPatente() == vehiculo.getPatente()) {
+            indiceArreglo = index;
+         }         
+      }
+      //no puedo modificar un vehiculo que no existe
+      if (indiceArreglo >= 0) {
+         this.vehiculos[indiceArreglo] = vehiculo;
+      } else {
+         console.log(`El vehiculo ${vehiculo.getPatente()} no se encuentra en el registro`);
+      }
+   }
+
+   eliminarVehiculo(vehiculo: Vehiculo):void {
+      let indiceArreglo = -1;
+      for (let index = 0; index < this.vehiculos.length; index++) {
+         const existente = this.vehiculos[index];
+         if (existente.getPatente() == vehiculo.getPatente()) {
+            indiceArreglo = index;
+         }         
+      }
+      //no puedo eliminar  un vehiculo que no existe
+      if (indiceArreglo >= 0) {
+         this.vehiculos.splice(indiceArreglo, 1);
+      } else {
+         console.log(`El vehiculo ${vehiculo.getPatente()} no se encuentra en el registro`);
+      }
+   }
 }
